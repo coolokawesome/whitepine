ee71fca3ada411e41c5050392c254a19d9fcf2c8
rickyjoe11 <48981536+rickyjoe11@users.noreply.github.com>
2023-02-26 12:49:01 -0500

    added sorting in gallery

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('working')
+title = $('#artistTitle')
+selector = $('#artistSelect')
+
+//main box
+contentRow= $('#contentRow')
+//options
+anne = $('#Anne')
+jeff = $('#Jeff')
+all = $('#All')
+
+
+selector.change(function () {
+    console.log(this.value);
+    //remove the header and content box
+    $('#artistHeader').remove()
+    $('#contentContainer').remove()
+    //title append
+    title.append(
+        $('<h3>').text(this.value).attr('id', 'artistHeader').attr('class', 'text-light')
+    )
+        //new box
+    contentRow.append(
+        $('<div>').attr('id', 'contentContainer')
+    )
+
+    $('#contentContainer').append(
+        $('<div>').attr('class', 'col-sm-6 col-lg-4 ').append(
+            $('<img>').attr('class', 'img img-fluid').attr('src', 'https://source.unsplash.com/random/?'+ this.value + ' ')
+        )
+    )
+});
